e0f7bae19d7f71edccc134016fa07af905e38978
Sebastiano Bea <sebafanclub@hotmail.it>
2020-03-28 17:32:18 -0700

    First Commit

---
+++ src/build/precache-manifest.3c841e1d191d631091938200de36c14b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f295cb22ee213b7f2636acb098f75953",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bf2dc8d87b42642da161",
+    "url": "/static/css/main.fe6824ef.chunk.css"
+  },
+  {
+    "revision": "0b32aaf695c352986680",
+    "url": "/static/js/2.73424c52.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.73424c52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf2dc8d87b42642da161",
+    "url": "/static/js/main.b7760b78.chunk.js"
+  },
+  {
+    "revision": "ab7d2e0a75360113bc06",
+    "url": "/static/js/runtime-main.e46617c8.js"
+  }
+]);
